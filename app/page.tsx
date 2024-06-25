d97af56c1727694acdb329ddc9b3d656ea39583a
Akaikenlol <keithken972@gmail.com>
2024-06-25 20:59:26 +0630

    feat: Implementing Footer and CreateCase components for landing page. chore: refactoring components.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Hero from "@/components/Hero";
 import Animated from "@/components/Animated";
 import Footer from "@/components/Footer";
+import CreateCase from "@/components/CreateCase";
 
 export default function Home() {
 	return (
 		<div className="bg-slate-50">
 			<Hero />
 			<Animated />
-			<Footer />
+			<CreateCase />
 		</div>
 	);
 }
