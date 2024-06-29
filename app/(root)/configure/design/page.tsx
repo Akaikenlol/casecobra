7b4dc65078da9cd844784b7f5f9edde9077ef3dc
Akaikenlol <keithken972@gmail.com>
2024-06-29 22:42:41 +0630

    feat: implementing configuration page chore: npm i headlessui, scroll-area, react-rnd, aspect-ratio

--- app/(root)/configure/design/page.tsx
+++ app/(root)/configure/design/page.tsx
@@ -1,7 +1,36 @@
+import DesignConfigurator from "@/components/DesignConfigurator";
+import { searchParamsProps } from "@/constants";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 import React from "react";
 
-const Page = () => {
-	return <div>Page</div>;
+const Page = async ({ searchParams }: searchParamsProps) => {
+	const { id } = searchParams;
+
+	if (!id || typeof id !== "string") {
+		return notFound();
+	}
+
+	const configuration = await db.configuration.findUnique({
+		where: { id },
+	});
+
+	if (!configuration) {
+		return notFound();
+	}
+
+	const { imageUrl, height, width } = configuration;
+
+	return (
+		<DesignConfigurator
+			configId={configuration.id}
+			imageUrl={imageUrl}
+			imageDimension={{
+				width,
+				height,
+			}}
+		/>
+	);
 };
 
 export default Page;
