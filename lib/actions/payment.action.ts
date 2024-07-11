c2867e87ac96ddf896fa290f1c80bf8077f99dc8
Akaikenlol <keithken972@gmail.com>
2024-07-11 23:53:54 +0630

    feat: Implementing Webhooks

--- lib/actions/payment.action.ts
+++ lib/actions/payment.action.ts
@@ -70,7 +70,7 @@ export const createCheckoutSession = async ({
 		cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
 		payment_method_types: ["card"],
 		mode: "payment",
-		shipping_address_collection: { allowed_countries: ["US", "JP", "BM"] },
+		shipping_address_collection: { allowed_countries: ["US", "JP", "MM"] },
 		metadata: {
 			userId: user.id,
 			orderId: order.id,
