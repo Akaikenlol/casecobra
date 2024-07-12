e795eb8b15a098eb4ae1f51ce6e9ebd11022b60e
Akaikenlol <keithken972@gmail.com>
2024-07-12 21:57:25 +0630

    chore: Remove unused imports in layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,4 @@
-import type { Metadata } from "next";
 import { Recursive } from "next/font/google";
-import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
