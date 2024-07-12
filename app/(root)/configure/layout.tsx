ce8bff287d2cff976fb953bb71384947541ec911
Akaikenlol <keithken972@gmail.com>
2024-07-12 22:03:44 +0630

    chore: Add Provider component to layout files

--- app/(root)/configure/layout.tsx
+++ app/(root)/configure/layout.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Provider from "@/components/Provider";
 import Steps from "@/components/Steps";
 import React from "react";
 
@@ -6,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<MaxWidthWrapper className="flex-1 flex flex-col min-h-screen no-scrollbar">
 			<Steps />
-			{children}
+			<Provider>{children}</Provider>
 		</MaxWidthWrapper>
 	);
 };
--- app/auth-callback/layout.tsx
+++ app/auth-callback/layout.tsx
@@ -1,3 +1,4 @@
+import Provider from "@/components/Provider";
 import React from "react";
 
 export const metadata = {
@@ -12,7 +13,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<Provider>{children}</Provider>
+			</body>
 		</html>
 	);
 }
