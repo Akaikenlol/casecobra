f334435f51092e0809847867f4612a709d854997
Akaikenlol <keithken972@gmail.com>
2024-07-13 21:56:19 +0630

    feat: Implementing ThankYou component to display order details

---
+++ app/(root)/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import ThankYou from "@/components/ThankYou";
+import React, { Suspense } from "react";
+
+const Page = () => {
+	return (
+		<Suspense>
+			<ThankYou />
+		</Suspense>
+	);
+};
+
+export default Page;
