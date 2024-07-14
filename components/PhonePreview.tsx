5495a457e746adf9cc29de6a75efc0ecc0b0e16e
Akaikenlol <keithken972@gmail.com>
2024-07-14 20:41:05 +0630

    feat: Update PhonePreview component with responsive image rendering

--- components/PhonePreview.tsx
+++ components/PhonePreview.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { CaseColor } from "@prisma/client";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AspectRatio } from "./ui/aspect-ratio";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 const PhonePreview = ({
 	croppedImageUrl,
@@ -18,8 +19,28 @@ const PhonePreview = ({
 		height: 0,
 		width: 0,
 	});
+
+	const handleResize = () => {
+		if (!ref.current) return;
+		const { width, height } = ref.current.getBoundingClientRect();
+		setRenderDimensions({ width, height });
+	};
+
+	useEffect(() => {
+		handleResize();
+
+		window.addEventListener("resize", handleResize);
+
+		return () => window.removeEventListener("resize", handleResize);
+	}, [ref.current]);
+
+	let caseBackgroundColor = "bg-zinc-950";
+	if (color === "blue") caseBackgroundColor = "bg-blue-950";
+	if (color === "red") caseBackgroundColor = "bg-rose-950";
+	if (color === "white") caseBackgroundColor = "bg-zinc-300";
+
 	return (
-		<AspectRatio ref={ref} ratio={8000 / 2001} className="relative">
+		<AspectRatio ref={ref} ratio={3000 / 2001} className="relative">
 			<div
 				className="absolute z-20 scale-[1.0352]"
 				style={{
@@ -28,7 +49,25 @@ const PhonePreview = ({
 					top: renderDimensions.height / 6.22,
 				}}
 			>
-				{/* <Image width={renderDimensions.width / (3000 / 637)} className='phone-skew relative z-20 rounded-t-[15px] rounded-b-[10px] md:rounded-t-[30px] md:rounded-b-[20px]'/> */}
+				<Image
+					width={renderDimensions.width / (3000 / 637)}
+					height={renderDimensions.height / 2001}
+					alt="Phone Image"
+					className={cn(
+						"phone-skew relative z-20 rounded-t-[15px] rounded-b-[10px] md:rounded-t-[30px] md:rounded-b-[20px]",
+						caseBackgroundColor
+					)}
+					src={croppedImageUrl}
+				/>
+			</div>
+
+			<div className=" relative h-full w-full z-40">
+				<Image
+					src={"/assets/clearphone.png"}
+					fill
+					alt="phone"
+					className="pointer-events-none w-full h-full antialiased rounded-md"
+				/>
 			</div>
 		</AspectRatio>
 	);
