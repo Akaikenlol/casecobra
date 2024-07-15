19f904fdf9604739833d24adfb7bb2e03253b0b6
Akaikenlol <keithken972@gmail.com>
2024-07-15 22:06:33 +0630

    feat: Add StatusDropDown component for changing order status

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -11,6 +11,18 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { formatPrice } from "@/lib/formatPrice";
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+
+import { Progress } from "@/components/ui/progress";
+import StatusDropDown from "@/components/StatusDropDown";
 
 const Page = async () => {
 	const { getUser } = getKindeServerSession();
@@ -49,6 +61,20 @@ const Page = async () => {
 			amount: true,
 		},
 	});
+	const lastMonthSum = await db.order.aggregate({
+		where: {
+			isPaid: true,
+			createdAt: {
+				gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+			},
+		},
+		_sum: {
+			amount: true,
+		},
+	});
+
+	const WEEKLY_GOAL = 500;
+	const MONTHLY_GOAL = 2500;
 
 	return (
 		<div className="flex min-h-screen w-full bg-muted/40">
@@ -62,8 +88,75 @@ const Page = async () => {
 									{formatPrice(lastWeekSum._sum.amount ?? 0)}
 								</CardTitle>
 							</CardHeader>
+							<CardContent>
+								<div className="text-sm text-muted-foreground">
+									of {formatPrice(WEEKLY_GOAL)} goal
+								</div>
+							</CardContent>
+							<CardFooter>
+								<Progress
+									value={((lastWeekSum._sum.amount ?? 0) * 100) / WEEKLY_GOAL}
+								/>
+							</CardFooter>
+						</Card>
+						<Card>
+							<CardHeader className="pb-2">
+								<CardDescription>Last Month</CardDescription>
+								<CardTitle className="text-4xl">
+									{formatPrice(lastMonthSum._sum.amount ?? 0)}
+								</CardTitle>
+							</CardHeader>
+							<CardContent>
+								<div className="text-sm text-muted-foreground">
+									of {formatPrice(MONTHLY_GOAL)} goal
+								</div>
+							</CardContent>
+							<CardFooter>
+								<Progress
+									value={((lastMonthSum._sum.amount ?? 0) * 100) / MONTHLY_GOAL}
+								/>
+							</CardFooter>
 						</Card>
 					</div>
+
+					<h1 className="text-4xl font-bold tracking-tight">Incoming Orders</h1>
+
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead>Customer</TableHead>
+								<TableHead className="hidden sm:table-cell">Status</TableHead>
+								<TableHead className="hidden sm:table-cell">
+									Purchase Date
+								</TableHead>
+								<TableHead className="text-right">Amount</TableHead>
+							</TableRow>
+						</TableHeader>
+
+						<TableBody>
+							{orders.map((order) => (
+								<TableRow className="bg-accent" key={order.id}>
+									<TableCell>
+										<div className="font-medium">
+											{order.shippingAddress?.name}
+										</div>
+										<div className="hidden text-sm text-muted-foreground md:inline">
+											{order.user.email}
+										</div>
+									</TableCell>
+									<TableCell className="hidden sm:table-cell">
+										<StatusDropDown id={order.id} orderStatus={order.status} />
+									</TableCell>
+									<TableCell className="hidden md:table-cell">
+										{order.createdAt.toLocaleDateString()}
+									</TableCell>
+									<TableCell className="text-right">
+										{formatPrice(order.amount)}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
 				</div>
 			</div>
 		</div>
